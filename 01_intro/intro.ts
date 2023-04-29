319113192996a26a91601c6ea7501e3053685b11
Abhishek Singh <aks3555@gmail.com>
2023-04-29 10:21:49 +0530

    Update intro.ts

--- 01_intro/intro.ts
+++ 01_intro/intro.ts
@@ -1,7 +1,11 @@
+// let's define an user object having name and age
 let user = {
     name: "Abhishek",
     age: 25
 }
 
-console.log("Abhishek");
+// this will work fine
+console.log(user.name); // Abhishek
+
+// if we pring user.email the IDE will not show any issue but we will be getting undefined.
 console.log(user.email);
