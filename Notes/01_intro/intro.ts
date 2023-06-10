bac04e5e19cecd7fe5975d2d8f51060f83490f2f
prochnost04 <aks3555@gmail.com>
2023-06-10 20:39:58 +0530

    restructure

--- Notes/01_intro/intro.ts
+++ Notes/01_intro/intro.ts
@@ -1,7 +1,11 @@
+// let's define an user object having name and age
 let user = {
     name: "Abhishek",
     age: 25
 }
 
-console.log("Abhishek");
-console.log(user.email);
+// this will work fine
+console.log(user.name); // Abhishek
+
+// if we print user.email the IDE will show us error.
+// console.log(user.email);
